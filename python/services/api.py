927c408e199bfc66f41178ae2108bb640888292d
David Contreras <davidcontrerasgarza@gmail.com>
2025-08-07 23:08:32 -0600

    Fix

--- python/services/api.py
+++ python/services/api.py
@@ -95,11 +95,10 @@ def apple_pay():
         negocio=json_data.get('negocio')
         id_cuenta=json_data.get('id_cuenta')
         id_categoria_de_gasto=json_data.get('id_categoria_de_gasto')
-        id_visualizacion=Gastos.query.filter_by(id_usuario=id_usuario).with_entities(func.max(Gastos.id_visualizacion)).scalar()
+        id_visualizacion=Gastos.query.filter_by(id_usuario=id_usuario).with_entities(func.max(Gastos.id_visualizacion)).scalar()+1
         new_record = Gastos(id_usuario=id_usuario,id_visualizacion=id_visualizacion,id_cuenta=id_cuenta,id_categoria_de_gasto=id_categoria_de_gasto,categoria_apple_pay=negocio,gasto_compartido='No',pagos_mensuales=1,importe=importe,fecha=fecha,negocio=negocio)
         db.session.add(new_record)
         db.session.commit() 
-        print('EXITO')
         return jsonify({
             'message': f'Gasto creado ID: {new_record.id}',
             'id': str(new_record.id)
